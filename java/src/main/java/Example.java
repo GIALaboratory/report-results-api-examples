85b26e30f2753cbd33bdfb3beb0540c0c691986b
Matthew Grogan <matt.grogan@gia.edu>
2019-10-28 17:36:42 -0400

    Finished example

--- java/src/main/java/Example.java
+++ java/src/main/java/Example.java
@@ -3,9 +3,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -53,8 +58,7 @@ public class Example {
         payload.put("query", query);
         payload.put("variables", query_variables);
 
-        // Use Gson builder for pretty-printing
-        //Gson gson = new Gson();
+        // Use Gson builder for pretty-printing, otherwise use: Gson gson = new Gson();
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         String json = gson.toJson(payload);
@@ -64,27 +68,82 @@ public class Example {
         System.out.println("---------------------------------------");
         System.out.println(json + "\n");
 
+        // Create http client and the post request
+        CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost(url);
         
+        // Set headers for the api key and content-type
         httpPost.setHeader("Authorization", key);
         httpPost.setHeader("Content-type", "application/json");
         
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        
         try {
+
+            // The payload is the JSON we created earlier
             httpPost.setEntity(new StringEntity(json));
-            CloseableHttpResponse response2 = httpclient.execute(httpPost);
-            System.out.println(response2.getStatusLine());
-            HttpEntity entity2 = response2.getEntity();
-            // do something useful with the response body
-            // and ensure it is fully consumed
-            System.out.println(EntityUtils.toString(entity2));
+
+            // Send the request to the server
+            CloseableHttpResponse response = httpclient.execute(httpPost);
+
+            // Retrieve the JSON result from the response
+            HttpEntity result = response.getEntity();
+            String jsonResult = EntityUtils.toString(result);
+
+
+            System.out.println("JSON RESPONSE RECEIVED FROM THE API");
+            System.out.println("-----------------------------------");
+            System.out.println(jsonResult + "\n");
+
+            // Parse the response (a string) into a JsonDocument so we can
+            // traverse the structure
+            JsonElement rootElement = JsonParser.parseString(jsonResult);
+
+            // Save all elements to a Map
+            Map<String, String> dict = new HashMap<String, String>();
+            flattenJsonDoc(rootElement, dict, "");
+
+            // Write all data to the console
+            System.out.println("PARSED REPORT RESULTS");
+            System.out.println("---------------------");
+
+            for (Entry<String, String> entry : dict.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
         
         } catch (Exception e) {
-            System.out.println("Error");
-            System.out.println(e);
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public static void flattenJsonDoc(JsonElement element, Map<String, String> dict, String path) {
+        // This method flattens a JSON document into a dictionary of strings
+        // element: The root element of the JSON to be flattened
+        // dict: The HashMap to receive the flattened elements
+
+        // A JSON object has properties that must be enumerated
+        if (element.isJsonObject()) {
+            for (Entry<String, JsonElement> el : element.getAsJsonObject().entrySet()) {
+                flattenJsonDoc(el.getValue(), dict, path + "/" + el.getKey());   
+            }
         }
 
+        // A JSON Array holds a number of JSON elements
+        if (element.isJsonArray()) {
+            int index = 0;
+            for (JsonElement child : element.getAsJsonArray()) {
+                flattenJsonDoc(child, dict, path + "/" + index);
+                index++;
+            }
+        }
 
+        // Check for null values
+        if (element.isJsonNull()) {
+            dict.put(path, element.getAsString());
+        }
+
+        // All others are JSON primitives
+        if (element.isJsonPrimitive()) {
+            dict.put(path, element.getAsString());
+        }
     }
 }
